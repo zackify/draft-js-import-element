aad8c7ba164c84a14480c1ba77b2eb666fe53506
Simon Sturmer <sstur@me.com>
2016-03-13 21:40:33 +0800

    better tests

--- src/__tests__/stateFromElement-test.js
+++ src/__tests__/stateFromElement-test.js
@@ -7,23 +7,64 @@ import {
   ElementNode,
 } from 'synthetic-dom';
 import {convertToRaw} from 'draft-js';
+import {jsdom} from 'jsdom';
+import fs from 'fs';
+import {join} from 'path';
+
+let document = jsdom('<!doctype html><html><body></body></html>');
+
+// This separates the test cases in `data/test-cases.txt`.
+const SEP = '\n\n#';
+
+let testCasesRaw = fs.readFileSync(
+  join(__dirname, '..', '..', 'test', 'test-cases.txt'),
+  'utf8',
+);
+
+let testCases = testCasesRaw.slice(1).trim().split(SEP).map((text) => {
+  let lines = text.split('\n');
+  let description = lines.shift().trim();
+  let state = removeBlockKeys(JSON.parse(lines[0]));
+  let html = lines.slice(1).join('\n');
+  return {description, state, html};
+});
 
 describe('stateFromElement', () => {
   let textNode = new TextNode('Hello World');
   let element = new ElementNode('div', [], [textNode]);
   it('should create content state', () => {
     let contentState = stateFromElement(element);
-    let rawContentState = convertToRaw(contentState);
-    let blocks = removeKeys(rawContentState.blocks);
-    expect(blocks).toEqual(
-      [{text: 'Hello World', type: 'unstyled', depth: 0, inlineStyleRanges: [], entityRanges: []}]
+    let rawContentState = removeBlockKeys(convertToRaw(contentState));
+    expect(rawContentState).toEqual(
+      {entityMap: {}, blocks: [{text: 'Hello World', type: 'unstyled', depth: 0, inlineStyleRanges: [], entityRanges: []}]}
     );
   });
 });
 
-function removeKeys(blocks) {
-  return blocks.map((block) => {
+describe('stateFromHTML', () => {
+  testCases.forEach((testCase) => {
+    let {description, state, html} = testCase;
+    it(`should render ${description}`, () => {
+      let element = parseHTML(html);
+      let actualState = removeBlockKeys(
+        convertToRaw(stateFromElement(element))
+      );
+      expect(actualState).toEqual(state);
+    });
+  });
+});
+
+function parseHTML(html: string): Element {
+  document.documentElement.innerHTML = html;
+  let body: Element = document.body;
+  return body;
+}
+
+function removeBlockKeys(content: Object): Object {
+  let newContent = {...content};
+  newContent.blocks = content.blocks.map((block) => {
     let {key, ...other} = block;
     return other;
   });
+  return newContent;
 }
